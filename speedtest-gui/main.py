48f6b9c125dc26f91311e835d3f4608a51a590e5
“Sergey” <s22-01-15@yandex.ru>
2024-08-19 03:09:22 +1000

    GUI tkinter & speedtest (add ping)

--- speedtest-gui/main.py
+++ speedtest-gui/main.py
@@ -7,9 +7,13 @@ def test():
     speed_test = speedtest.Speedtest()
     download_speed = round(speed_test.download() / (1024 ** 2), 2)
     upload_speed = round(speed_test.upload() / (1024 ** 2), 2)
+    servernames =[]
+    speed_test.get_servers(servernames)
+    ping_ms = speed_test.results.ping
 
-    download_lable.config(text="Скорость загрузки:\n-" + str(download_speed) + "MbPs")
-    upload_lable.config(text="Скорость отдачи:\n-" + str(upload_speed) + "MbPs")
+    download_lable.config(text="Скорость загрузки:\n" + str(download_speed) + "MbPs")
+    upload_lable.config(text="Скорость отдачи:\n" + str(upload_speed) + "MbPs")
+    ping_lable.config(text="Ping:\n" + str(ping_ms) + "ms")
 
 root = Tk()
 
@@ -23,5 +27,7 @@ download_lable = Label(root, text="Скорость загрузки:\n-", font=
 download_lable.pack(pady=(50, 0))
 upload_lable = Label(root, text="Скорость отдачи:\n-", font=35)
 upload_lable.pack(pady=(10, 0))
+ping_lable = Label(root, text="Ping:\n-", font=35)
+ping_lable.pack(pady=(10, 0))
 
 root.mainloop()
